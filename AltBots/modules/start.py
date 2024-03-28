c3a033fb09b76a2e3b5c6622c5d85f9a5f886984
Ketannama <165205149+Ketannama@users.noreply.github.com>
2024-03-28 13:34:36 +0530

    Update start.py

--- AltBots/modules/start.py
+++ AltBots/modules/start.py
@@ -5,15 +5,15 @@ from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 
 START_BUTTON = [
     [
-        Button.url(" 𝐌ᴜsɪᴄ ", "https://t.me/HINATA_N_BOT"),
-        Button.url("𝐎ᴡɴᴇʀ", "https://t.me/Saif_Dictator")
+        Button.url(" 𝐌ᴜsɪᴄ ", "https://t.me/+6zxsO6dEG0owNTg1"),
+        Button.url("𝐎ᴡɴᴇʀ", "https://t.me/+6zxsO6dEG0owNTg1")
     ],
     [
-        Button.url(" 𝐂н𝙰𝙽𝙽𝙴𝙻 ", "https://t.me/SAIFALLBOT"),
-        Button.url(" 𝐒𝚄𝙿𝙿𝙾𝚃  ", "https://t.me/SAIFHELPGC")
+        Button.url(" 𝐂н𝙰𝙽𝙽𝙴𝙻 ", "https://t.me/+6zxsO6dEG0owNTg1"),
+        Button.url(" 𝐒𝚄𝙿𝙿𝙾𝚃  ", "https://t.me/+6zxsO6dEG0owNTg1")
     ],
     [
-        Button.url("𝐑ᴇᴘᴏ ", "github.com/SAIFDEAD/AXSPAMBOT"),
+        Button.url("𝐑ᴇᴘᴏ ", "https://github.com/Ketannama/AXSPAMBOT"),
       
     ]
 ]
@@ -36,13 +36,13 @@ async def start(event):
         bot_name = AltBot.first_name
         bot_id = AltBot.id
         TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝐒ᴀsᴜᴋᴇ](https://t.me/Saif_Dictator)**\n\n"
+        TEXT += f"» **my developer : [sanatani](https://t.me/+6zxsO6dEG0owNTg1)**\n\n"
         TEXT += f"» **ᴅᴇᴀᴅ ᴠᴇʀsɪᴏɴ :** `M3.3`\n"
         TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
         TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
         await event.client.send_file(
                     event.chat_id,
-                    "https://graph.org/file/eb514f6d4b35027f8fe0b.jpg",
+                    "https://telegra.ph/file/42398ed35462dc506af19.jpg",
                     caption=TEXT, 
                     buttons=START_BUTTON
 )
